ab70040dc6993b7490b9ea1f2eb320ab1207c280
Alex Puschinsky <alexpoo@gmail.com>
2018-05-12 17:36:58 +0300

    Create project

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const PostCompile = require('post-compile-webpack-plugin');
+const axios = require('axios');
+
+module.exports = {
+  entry: ['./src/extension/src/inject/inject.js'],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: './src/extension/manifest.json', to: '' },
+      { from: './src/extension/src/bg/background.js', to: '' },
+      { from: './src/extension/icons', to: 'icons' }
+    ])
+  ]
+};
