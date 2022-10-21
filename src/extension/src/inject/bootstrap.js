9b0a8c3e2494ef214758fa883c91b313f2da373c
Alex Puschinsky <alexpoo@gmail.com>
2022-10-22 00:55:28 +0300

    Fix blank page after login

--- src/extension/src/inject/bootstrap.js
+++ src/extension/src/inject/bootstrap.js
@@ -1,18 +1,39 @@
 // global var used by rabbitmq code
-extension_count++;
-NAVIGATION.Awesome = ["#/awesome", "management"];
-dispatcher_add(sammy => {
-  sammy.get("/awesome", context => {
-    current_highlight = "#/awesome";
-    update_navigation();
-    replace_content("main", "<div id='awesome-root'></div>");
 
-    const message = {
-      source: "awesome",
-      authHeader: auth_header(),
-      timerInterval: window.timer_interval
-    };
+waitForLogin(() => {
+  extension_count++;
+  NAVIGATION.Awesome = ["#/awesome", "management"];
+  dispatcher_add(sammy => {
+    sammy.get("/awesome", context => {
+      current_highlight = "#/awesome";
+      update_navigation();
+      replace_content("main", "<div id='awesome-root'></div>");
 
-    window.postMessage(message, "*");
+      const message = {
+        source: "awesome",
+        authHeader: auth_header(),
+        timerInterval: window.timer_interval
+      };
+
+      window.postMessage(message, "*");
+    });
   });
-});
+})
+
+/**
+ * If try to add the awesome tab on the login page, it will cause a black page
+ */
+function waitForLogin(cb) {
+  const pollMenuVisible = () => {
+    const isLoggedIn = !!window.user;
+
+    if(isLoggedIn) {
+      cb();
+      return;
+    } 
+
+    setTimeout(pollMenuVisible, 100);
+  }
+
+  pollMenuVisible(cb);
+}
